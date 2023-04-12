9b449043c44f86369ad3c1fc3af09c6fceb5cfec
gitchaelin <67990769+gitchaelin@users.noreply.github.com>
2023-04-12 13:30:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 alert(1);
+
+function get_cookie(name) {
+    var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return value? value[2] : null;
+}
+
+alert(get_cookie("flag"))
